6a116e3edc749768fdd1fbd562f8037ad1fc44db
Fredrick Garfield <44140633+FredrickGarfield@users.noreply.github.com>
2023-12-16 08:18:22 +0000

    Modify

--- sw.js
+++ sw.js
@@ -4,4 +4,4 @@ importScripts("res/the.js");
 
 const sw = new UVServiceWorker();
 
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener("fetch", event => event.respondWith(sw.fetch(event)));
